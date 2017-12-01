792f7ef480c08978d3c06c6193857e30365f4d1c
Michael <michaelo1@gmx.de>
2017-12-01 21:11:48 +0100

    File Template for AoC-Days added

---
+++ .idea/fileTemplates/Advent of Code - Day.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ${NAME} {
+
+    private static final String myInput = "";
+
+    ${NAME}() {
+        System.out.println("\n--- ${NAME}: ____ ---");
+        // input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter Input-String: ");
+        String input = null;
+        try {
+            input = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (input == null || "".equals(input)) {
+            input = myInput;
+        }
+        // wörk
+        // TODO: ${DATE} work with input 
+        System.out.println();
+        // finished
+    }
+
+    public static void main(String[] args) {
+        new ${NAME}();
+    }
+}
